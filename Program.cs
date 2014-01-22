498daea231863d24298cb6dfbb64440f4adf19bb
tort32 <tort.biz@gmail.com>
2014-01-22 19:19:36 +0400

    solution 11
    failed

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace ProjectEuler
             var watch = new System.Diagnostics.Stopwatch();
             watch.Start();
 
-            ProblemBase problem = new Problem10();
+            ProblemBase problem = new Problem11();
             Decimal solution = problem.Solve();
 
             watch.Stop();
